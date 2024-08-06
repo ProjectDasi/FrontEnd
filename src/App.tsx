1f1eeb670aa235b0a421533a1c16a5429d543f9e
AwesomeJuneKim <900617w@naver.com>
2024-08-06 11:52:11 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-slate-500">
+      react
+    </div>
+  );
+}
+
+export default App;
